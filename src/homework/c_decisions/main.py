c60cc21dec037f1552beb5ca77682251c84b75af
WillCapo <william.capozzoli@g.austincc.edu>
2022-02-16 00:17:47 -0600

    Homework 2 complete

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,12 @@
-#
+import decisions
+
+print('Please enter a number for \'Options\'')
+options = int(input())
+
+print('Please enter a number for \'Total\' (NOTE: Number must be larger than \'Options\')')
+total = int(input())
+
+result = decisions.get_options_ratio(options, total)
+strResult = str(result)
+
+print('The rating is ' + strResult + ' which is ' + decisions.get_faculty_rating(result))
--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,11 @@
-#
+import unittest
+
+from src.homework.c_decisions.decisions import get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class Test_Config(unittest.TestCase):
+    def test_options_ratio(self):
+        self.assertEqual(get_options_ratio(5, 20), .25)
+    
+    def test_faculty_rating(self):
+        self.assertEqual(get_faculty_rating(.71), 'Good')
